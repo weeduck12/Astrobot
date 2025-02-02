e53b34b452a4646ac72bf70f40922f2e8ed4bad3
weeduck12 <bouna135g@gmail.com>
2025-02-02 22:12:25 +0100

    changement mineur

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"astrobot awakened")
+    print(f"astrobot est réveillé")
 
 @bot.event
 async def on_message(message):
