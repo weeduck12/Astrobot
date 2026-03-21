54627b8668c465a27f35adacf4084dc5e98fa276
weeduck12 <bouna135@hotmail.fr>
2026-03-21 20:34:19 +0100

    cogs added &  code move

--- bot.py
+++ bot.py
@@ -1,32 +1,12 @@
 import discord 
-from discord.ext import commands
 import os
 from dotenv import load_dotenv
-import time
 
-#charger le token
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
-#intents 
-intents = discord.Intents.default()
-intents.messages = True
-intents.message_content = True
-#Création du bot 
-bot = commands.Bot(command_prefix="!", intents=intents)
-
-@bot.event
+client = discord.Client()
+@client.event
 async def on_ready():
-    print(f"astrobot est réveillé")
-
-@bot.event
-async def on_message(message):
-    if message.author.bot:
-        return
-
-    if "quoi" in message.content.lower():
-        await message.channel.send("feur")
-
-        await bot.process_commands(message)
-
-bot.run(TOKEN)
+    print(f"{client.user} is booting....")
+client.run(TOKEN)
 
